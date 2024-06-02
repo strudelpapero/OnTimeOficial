480af0044f7e14520674d08814499e66a3111c5f
trinidagos <110606466+trinidagos@users.noreply.github.com>
2024-06-02 16:27:58 -0300

    prueba tri

--- BasedeDatos.sql
+++ BasedeDatos.sql
@@ -1,9 +1,10 @@
-impo
+CREATE DATABASE IF NOT EXISTS ontime;
 
+USE ontime;
 
-CREATE TABLE Plato
+CREATE TABLE IF NOT EXISTS Plato
 (
-IdPlato int primary key not null,
+IdPlato int AUTO_INCREMENT primary key,
 Nombre varchar (60) not null,
 Imagen blob not null, 
 Descripcion text not null,
@@ -13,3 +14,4 @@ Vegano boolean,
 Kosher boolean
 );
 
+INSERT INTO Plato (Nombre, Descripcion, Precio, Celiaco, Vegano, Kosher) VALUES ('Sandwich de pollo', 'Pollo grillado con miel, limon, y mostaza de dijon, hongos salteados, cebolla caramelizada, cherry confitado con ajo y queso parmesano en pan de campo de molde', 9900, False, False, False);
--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const mysql = require('mysql2/promise')
 const app = express();
 const port = process.env.PORT || 3000;
 
-MYSQL_HOST = 'https://on-time-oficial-git-main-jeronimos-proyects-dcccfe57.vercel.app/'
-MYSQL_PORT = 3306
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = port
 MYSQL_USER = 'root'
 MYSQL_PASSWARD = 'OnTime'
 MYSQL_DATABASE = 'ontime'
 
 app.get('/', async (req, res) => {
-    try {
+    //try {
         const connection = await mysql.createConnection({
-            host: process.env.MYSQL_HOST,
-            port: process.env.MYSQL_PORT,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWARD,
-            database: process.env.MYSQL_DATABASE,
+            host: MYSQL_HOST,
+            port: MYSQL_PORT,
+            user: MYSQL_USER,
+            password: MYSQL_PASSWARD,
+            database: MYSQL_DATABASE,
         });
 
-        const [row, fields] = await connection.execute('SELECT 1 + 1 AS solution');
-            res.send(`La solucion es: ${rows[0].solution}`)
-            await connection.end();
-    }catch (error) {
-        res.status(500).send('Database connection failed')
-    }
+        //const [row, fields] = await connection.execute('SELECT 1 + 1 AS solution');
+            //res.send(`La solucion es: ${rows[0].solution}`)
+            //await connection.end();
+    //}catch (error) {
+        //res.status(500).send('Database connection failed')
+    //}
 });
 
 app.listen(port, () => {
