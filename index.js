7cbef5a7c394c3770fd4cef203b9e86cf3373d2b
jeronimo elbaum <47569893@est.ort.edu.ar>
2024-06-04 15:46:31 -0300

    funcionamiento vercel

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const express = require("express");
-const mysql = require('mysql2/promise')
+const { Client } = require('pg');
 const app = express();
 const port = process.env.PORT || 9000;
 
-POSTGRES_URL="postgres://default:rZAb68xzXTNI@ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-POSTGRES_PRISMA_URL="postgres://default:rZAb68xzXTNI@ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
-POSTGRES_URL_NO_SSL="postgres://default:rZAb68xzXTNI@ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech:5432/verceldb"
-POSTGRES_URL_NON_POOLING="postgres://default:rZAb68xzXTNI@ep-tight-sea-a4v1n2xf.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-POSTGRES_USER="default"
-POSTGRES_HOST="ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech"
-POSTGRES_PASSWORD="rZAb68xzXTNI"
-POSTGRES_DATABASE="verceldb"
+const POSTGRES_URL = "postgres://default:rZAb68xzXTNI@ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+const POSTGRES_USER = "default";
+const POSTGRES_HOST = "ep-tight-sea-a4v1n2xf-pooler.us-east-1.aws.neon.tech";
+const POSTGRES_PASSWORD = "rZAb68xzXTNI";
+const POSTGRES_DATABASE = "verceldb";
+
 app.get('/', async (req, res) => {
     try {
-        const connection = await mysql.createConnection({
-            host: POSTGRES_HOST,
-            user: POSTGRES_USER,
-            password: POSTGRES_PASSWORD,
-            database: POSTGRES_DATABASE,
+        const client = new Client({
+            connectionString: POSTGRES_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
         });
-        const [row, fields] = await connection.execute('SELECT 1 + 1 AS solution');
-            res.send(`La solucion es: ${rows[0].solution}`)
-            await connection.end();
-    }catch (error) {
-        res.status(500).send('Database connection failed')
+
+        await client.connect();
+        const result = await client.query('SELECT 1 + 1 AS solution');
+        res.send(`La solución es: ${result.rows[0].solution}`);
+        await client.end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Database connection failed');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server running at `);
-})
+    console.log(`Server running at port ${port}`);
+});
