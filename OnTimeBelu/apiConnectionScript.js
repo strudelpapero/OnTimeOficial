17d6a35a4cc4a6fcbd9891edf60f8b5b6c35bc0b
jeronimo elbaum <47569893@est.ort.edu.ar>
2024-07-02 11:17:11 -0300

    conexion hecha

--- OnTimeBelu/apiConnectionScript.js
+++ OnTimeBelu/apiConnectionScript.js
@@ -8,9 +8,30 @@ const getDishes = async () => {
         }
         const data = await response.json(); // Parsea la respuesta a JSON
         console.log(data);
+        renderDishes(data); // Llama a la función para renderizar los datos
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 };
 
+const renderDishes = (dishes) => {
+    const menuItemsContainer = document.getElementById('menu-items');
+    menuItemsContainer.innerHTML = ''; // Limpia el contenedor antes de agregar los nuevos elementos
+
+    dishes.forEach(dish => {
+        const dishElement = document.createElement('div');
+        dishElement.classList.add('plato');
+
+        dishElement.innerHTML = `
+            <span>${dish.nombre}</span>
+            <div class="info">
+                <button type="button" class="mas-info-btn">
+                    <img class="mas-info" src="Assets/icons/+.svg" alt="Mas informacion">
+                </button>
+                <span id="precio">$${dish.precio}</span>
+            </div>
+        `;
+        menuItemsContainer.appendChild(dishElement);
+    });
+};
 getDishes();
