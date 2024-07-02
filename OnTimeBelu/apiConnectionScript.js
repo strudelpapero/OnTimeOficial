5e30524abba078b9b6c82a9a6f0c2190f974f682
jeronimo elbaum <47569893@est.ort.edu.ar>
2024-07-02 16:08:37 -0300

    conexion, abre el popup

--- OnTimeBelu/apiConnectionScript.js
+++ OnTimeBelu/apiConnectionScript.js
@@ -25,7 +25,7 @@ const renderDishes = (dishes) => {
         dishElement.innerHTML = `
             <span>${dish.nombre}</span>
             <div class="info">
-                <button type="button" class="mas-info-btn">
+                <button type="button" class="mas-info-btn" data-nombre="${dish.nombre}" data-descripcion="${dish.descripcion}" data-imagen="${dish.imagen}">
                     <img class="mas-info" src="Assets/icons/+.svg" alt="Mas informacion">
                 </button>
                 <span id="precio">$${dish.precio}</span>
@@ -33,5 +33,29 @@ const renderDishes = (dishes) => {
         `;
         menuItemsContainer.appendChild(dishElement);
     });
+
+    // Añadir event listeners a los nuevos botones mas-info-btn
+    document.querySelectorAll('.mas-info-btn').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const nombre = event.currentTarget.getAttribute('data-nombre');
+            const descripcion = event.currentTarget.getAttribute('data-descripcion');
+            const imagen = event.currentTarget.getAttribute('data-imagen');
+
+            document.querySelector('.box-plato').textContent = nombre;
+            document.querySelector('.box-info-plato').textContent = descripcion;
+            document.querySelector('.box-foto-plato').src = imagen;
+
+            // Mostrar el contenedor de la caja de información
+            document.getElementById('conteinerInfoBox').classList.add('show');
+        });
+    });
 };
+
+// Añadir event listener al botón de cerrar después de que el DOM esté listo
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('closeInfoBox').addEventListener('click', () => {
+        document.getElementById('conteinerInfoBox').classList.remove('show');
+    });
+});
+
 getDishes();
