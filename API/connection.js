af47b407df0624a08cab3d2333e6276ad02f07af
jeronimo elbaum <47569893@est.ort.edu.ar>
2024-08-13 10:28:02 -0300

    nacho esta loco

--- API/connection.js
+++ API/connection.js
@@ -1,26 +1,28 @@
-const { Pool } = require('pg');
-const express = require('express')
-const cors = require('cors'); // Importa el paquete cors
-const open = require('open'); // Importa el paquete open
+import pg from 'pg';
+const { Pool } = pg;
+import express from 'express';
+import cors from 'cors'; // Importa el paquete cors
 
-require('dotenv').config();
+import "dotenv/config";
 
-console.log("POSTGRES_URL:", process.env.POSTGRES_URL); 
+console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.POSTGRES_URL,
 });
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://127.0.0.1:5500' })); // Usa cors y permite solicitudes desde localhost:5050
+app.use(cors({ origin: 'http://127.0.0.1:5500' })); // Usa cors y permite solicitudes desde localhost:5500
 
 const port = 3000;
 
+// Endpoint para obtener platos
 app.get('/getDishes', async (req, res) => {
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM Platos');
+        client.release(); // Libera el cliente después de la consulta
         res.json(result.rows);
     } catch (err) {
         console.error('Error fetching data:', err);
@@ -28,6 +30,7 @@ app.get('/getDishes', async (req, res) => {
     }
 });
 
+// Inicia el servidor en el puerto especificado
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
