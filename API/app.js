98cb6a809c746bba5e16f8017b5bd92e6ab9ffe2
trinidagos <110606466+trinidagos@users.noreply.github.com>
2024-08-23 12:07:19 -0300

    rest y plato intento query

---
+++ API/app.js
@@ -0,0 +1,59 @@
+// app.js
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+app.get('/getDishes', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM Platos');
+    client.release();
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.get('/getInfo_RestyPlato/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`SELECT 
+      r.nombre AS restaurante,
+      r.direccion,
+      r.calle,
+      p.nombre AS plato,
+      p.precio
+  FROM 
+      restaurante r
+  JOIN 
+      platos p ON r.id = p.id_rest
+  WHERE 
+      r.id = $1;
+  ` [id]);
+    client.release();
+    res.json(result.rows);
+    print(result.rows);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.post()
+//Ejemplo cuando se accede a /ping devulve /pong
+app.get("/ping", (req, res) => {
+  res.json("pong")
+} )
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
