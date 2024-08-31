3d1c310366973cfed70b4450a2936512bcdceb5f
trinidagos <trinidagos@gmail.com>
2024-08-30 22:28:30 -0300

    post

--- src/app/api/getInfo_RestyPlatos/[id]/route.ts
+++ src/app/api/getInfo_RestyPlatos/[id]/route.ts
@@ -1,26 +1,27 @@
 import { sql } from '@vercel/postgres';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: Params }) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id; // Captura el parámetro 'id' de la URL
   try {
-    const { id } = params;
-    // Ejecutar la consulta
-    const result = await sql`SELECT 
+    const result = await sql`
+      SELECT 
         r.nombre AS restaurante,
         r.direccion,
         r.calle,
-        r.foto
+        r.foto,
         p.nombre AS plato,
         p.precio
-    FROM 
+      FROM 
         restaurante r
-    JOIN 
+      JOIN 
         platos p ON r.id = p.id_rest
-    WHERE 
-        r.id = ${id};`;
+      WHERE 
+        r.id = ${id};
+    `;
     // Devolver los resultados en formato JSON con un estado 200 (OK)
     return NextResponse.json(result.rows, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     // Manejo de errores
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
