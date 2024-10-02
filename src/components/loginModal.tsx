3880c5277f8a7cab158af8cce6870b0241333d1c
beludominguezz <47699504@est.ort.edu.ar>
2024-10-02 13:51:51 -0600

    esta el login, instalar las dependencias, configurar el proyecto en google cloud (chati)

--- src/components/loginModal.tsx
+++ src/components/loginModal.tsx
@@ -1,15 +1,50 @@
 "use client";  // ← Añade esto al inicio del archivo
-
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import React from 'react';
+import { useState } from 'react';
+import axios from "axios"
+import Cookies from 'js-cookie'
 
-interface LoginModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
 
-const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
+const LoginModal = ({ isOpen, onClose }) => {
+
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const authenticateUser = (user, pass) => {
+    const userData = {
+      user,
+      pass
+    }
+    const expirationTime = new Date (new Date().getTime() + 600000)
+    Cookies.set('auth', JSON.stringify(userData), {expires: expirationTime})
+  }
+
+
   if (!isOpen) return null;
 
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (response) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${response.access_token}`
+            }
+          }
+        );
+        setUsername(res.data.name)
+        setPassword(res.data.password)
+        authenticateUser(username, password)
+      }catch (err){
+        console.log(err)
+      }
+    }
+  })
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content-login" onClick={(e) => e.stopPropagation()}>
@@ -21,6 +56,13 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
           <form>
             <input type="text" placeholder="Nombre" />
             <input type="password" placeholder="Contraseña" />
+            <GoogleLogin
+              onSuccess={() => googleLogin()}
+              onError={() => {
+              console.log('Login failed')
+            }}
+           
+            />
             <button type="submit">INGRESAR</button>
           </form>
         </div>
@@ -29,4 +71,8 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
   );
 };
 
+
 export default LoginModal;
+
+
+
